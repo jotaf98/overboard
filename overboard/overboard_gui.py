020c1be5abbf29b57ae5c188894a4960b86c3dd9
Joao Henriques <jotaf98@hotmail.com>
2019-04-06 13:11:43 +0100

    update cmd line arguments and their help text

--- overboard/overboard_gui.py
+++ overboard/overboard_gui.py
@@ -22,11 +22,13 @@ from .visualizations import Visualizations
 def main():
   # parse command line options
   parser = argparse.ArgumentParser()
-  parser.add_argument("folder")
-  #parser.add_argument("-dpi", default=100, type=int)
-  parser.add_argument("-plotsize", default=0, type=float)
+  parser.add_argument("folder", help="Root folder where experiments are found.")
+  parser.add_argument("-plotsize", default=0, type=float, help="Initial size of plots, in pixels.")
   #parser.add_argument("-smoothen", default=0, type=float)
-  parser.add_argument('--force-reopen-files', action='store_true', default=False)
+  parser.add_argument("--force-reopen-files", action='store_true', default=False, help="Slower but more reliable refresh method, useful for remote files.")
+  parser.add_argument("-refresh-plots", default=1999, type=int, help="Refresh interval for plot updates, in miliseconds.")
+  parser.add_argument("-refresh-new", default=3999, type=int, help="Refresh interval for finding new experiments, in miliseconds.")
+  parser.add_argument("-refresh-vis", default=2999, type=int, help="Refresh interval for visualizations, in miliseconds.")
   args = parser.parse_args()
   
   # find experiment files
@@ -51,17 +53,17 @@ def main():
   # create timer for updating the plots periodically if needed
   plot_timer = QtCore.QTimer()
   plot_timer.timeout.connect(partial(plots.update_plots, experiments))
-  plot_timer.start(1999)
+  plot_timer.start(args.refresh_plots)
   
   # create timer to check for new experiments
   new_exp_timer = QtCore.QTimer()
   new_exp_timer.timeout.connect(partial(check_new_experiments, experiments, set(files), args.folder, window, args.force_reopen_files))
-  new_exp_timer.start(3999)
+  new_exp_timer.start(args.refresh_new)
   
   # create timer for updating the current visualizations
   vis_timer = QtCore.QTimer()
   vis_timer.timeout.connect(visualizations.update)
-  vis_timer.start(2999)
+  vis_timer.start(args.refresh_vis)
 
   window.show()
   
