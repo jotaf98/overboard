950b14ec0b2b4798698625e9714c203fb6cb6c69
Joao Henriques <jotaf98@hotmail.com>
2019-05-28 15:06:20 +0100

    better logging and handling of crashes

--- overboard/overboard_gui.py
+++ overboard/overboard_gui.py
@@ -1,5 +1,5 @@
 
-import sys, argparse, glob, collections
+import sys, argparse, glob, collections, logging
 from functools import partial
 
 #import os
@@ -31,13 +31,23 @@ def main():
   parser.add_argument("-refresh-new", default=3999, type=int, help="Refresh interval for finding new experiments, in miliseconds.")
   parser.add_argument("-refresh-vis", default=2999, type=int, help="Refresh interval for visualizations, in miliseconds.")
   parser.add_argument("--no-vis-snapshot", action='store_true', default=False, help="Visualizations are draw using a snapshot of the visualization function, saved with each experiment. This ensures visualizations from old experiments are maintained. Passing this option disables this behavior, which may be useful for debugging.")
+  parser.add_argument("--debug", action='store_true', default=False, help="Does not suppress exceptions during operation, useful for debugging.")
   args = parser.parse_args()
+
+  # create an exception hook, to avoid GUI exiting on uncaught exceptions
+  if not args.debug:
+    def trap_exceptions(err_type, err_value, traceback):
+      logging.exception('Uncaught exception ' + str(err_type), exc_info=err_value)
+    sys.excepthook = trap_exceptions
   
   # find experiment files
+  logging.info('Finding experiments...')
   files = glob.glob(args.folder + "/**/stats.csv", recursive=True)
 
   # load the experiments
+  logging.info('Loading experiments...')
   experiments = [Experiment(filename, args.folder, args.force_reopen_files) for filename in files]
+  logging.info('Done.')
 
   # create Qt application
   app = QtWidgets.QApplication(sys.argv)
@@ -51,9 +61,6 @@ def main():
   # create initial plots for all the experiments
   for exp in experiments:
     window.add_experiment(exp, refresh_table=False)
-  
-  if len(experiments) > 0:  # select first row if any
-    window.table.selectRow(0)
   
   # create timer for updating the plots periodically if needed
   plot_timer = QtCore.QTimer()
@@ -74,6 +81,9 @@ def main():
   
   window.sort_table_by_timestamp(refresh_table=True)
 
+  if window.table.rowCount() > 0:  # select first row if any (after sorting)
+    window.table.selectRow(0)
+  
   app.exec_()
   #sys.exit(app.exec_())
 
